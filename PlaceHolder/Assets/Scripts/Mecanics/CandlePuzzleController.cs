a5519a8519ad43e000f7121755fe8f8fe2ad9715
sovamisha1 <80858032+sovamisha1@users.noreply.github.com>
2024-06-12 23:47:01 +0300

    Добавил прототип взаимодействия с предметами и прототип головоломки со свечками

---
+++ PlaceHolder/Assets/Scripts/Mecanics/CandlePuzzleController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandlePuzzleController : MonoBehaviour
+{
+    public List<CandleForPuzzle> candles;
+    public List<int> correctOrder;
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        correctOrder = new List<int>() { 1, 2, 3 };
+        if (correctOrder.Count != candles.Count)
+        {
+            Debug.LogError("Количество элементов в correctOrder и candles должно совпадать!");
+        }
+    }
+
+    public void LightCandle(int torchIndex)
+    {
+        if (torchIndex == correctOrder[currentIndex])
+        {
+            candles[torchIndex].LitCandle();
+            currentIndex++;
+            Debug.Log("Правильно");
+
+            if (currentIndex >= correctOrder.Count)
+            {
+                Debug.Log("Головоломка решена!");
+            }
+        }
+        else
+        {
+            Debug.Log("АНЛАКИ");
+            ExtinguishAllcandles();
+            currentIndex = 0;
+        }
+    }
+
+    private void ExtinguishAllcandles()
+    {
+        foreach (var torch in candles)
+        {
+            torch.Extinguish();
+        }
+    }
+}
---
+++ PlaceHolder/Assets/Scripts/Mecanics/Interacteble.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour, IInteracteble
+{
+    public virtual void Interact()
+    {
+        Debug.Log("Interacted with " + gameObject.name);
+    }
+}
