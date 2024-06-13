72fc0ebbeb9471acbe0908ab0d3e220a64451c69
sovamisha1 <80858032+sovamisha1@users.noreply.github.com>
2024-06-13 17:02:57 +0300

    Добавлены префаб посоха и подсказка при наведении на предмет с которым можно взаимодействовать

---
+++ PlaceHolder/Assets/Scripts/Mecanics/Staf.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Staf : Interactable
+{
+    private Inventory inventory;
+
+    void Start()
+    {
+        inventory = FindObjectOfType<Inventory>();
+    }
+
+    public override void Interact()
+    {
+        inventory.AddItem("Посох Мага");
+        Destroy(gameObject);
+    }
+}
